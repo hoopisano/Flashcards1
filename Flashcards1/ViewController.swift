9abfabb8b64eae9e66cb711180d78395355b5ba0
Andrew Pisano <andrewpisano3@gmail.com>
2021-03-19 10:29:40 -0400

    Re-submission Week 4

--- Flashcards1/ViewController.swift
+++ Flashcards1/ViewController.swift
@@ -8,12 +8,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var frontLabel: UILabel!
+    @IBOutlet weak var backLabel: UILabel!
+    @IBOutlet weak var card: UIView!
+    
+    @IBOutlet weak var btnOptionOne: UIButton!
+    @IBOutlet weak var btnOptionTwo: UIButton!
+    @IBOutlet weak var btnOptionThree: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        card.layer.cornerRadius = 20.0
+        card.layer.shadowRadius = 25.0
+        card.layer.shadowOpacity = 0.3
+        
+        frontLabel.layer.cornerRadius = 20.0
+        frontLabel.layer.shadowOpacity = 0.2
+        frontLabel.layer.shadowRadius = 15.0
+        frontLabel.clipsToBounds = true
+        
+        backLabel.layer.cornerRadius = 20.0
+        backLabel.layer.shadowOpacity = 0.2
+        backLabel.layer.shadowRadius = 15.0
+        backLabel.clipsToBounds = true
+        
+        btnOptionOne.layer.cornerRadius = 20.0
+        btnOptionOne.layer.borderWidth = 3.0
+        btnOptionOne.layer.borderColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        
+        btnOptionTwo.layer.cornerRadius = 20.0
+        btnOptionTwo.layer.borderWidth = 3.0
+        btnOptionTwo.layer.borderColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        
+        btnOptionThree.layer.cornerRadius = 20.0
+        btnOptionThree.layer.borderWidth = 3.0
+        btnOptionThree.layer.borderColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        
     }
 
-
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let navigationController = segue.destination as! UINavigationController
+        let creationController = navigationController.topViewController as! CreationViewController
+        creationController.flashcardsController = self
+    }
+    
+    func updateFlashcard(question: String, answer: String) {
+        frontLabel.text = question
+        backLabel.text = answer
+    }
+    
+    @IBAction func didTapOptionOne(_ sender: Any) {
+    }
+    
+    @IBAction func didTapOptionTwo(_ sender: Any) {
+        frontLabel.isHidden = !frontLabel.isHidden
+    }
+    
+    @IBAction func didTapOptionThree(_ sender: Any) {
+    }
+    
 }
 
